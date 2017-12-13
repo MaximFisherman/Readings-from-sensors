12a9e0423a30b702897b81a69dd3d6fabe2b8230
Maxim <maksimrybak666@gmail.com>
2017-12-13 17:25:02 +0200

    Testing delete function mongoDB

--- Reading for sensors/MongoDB.cpp
+++ Reading for sensors/MongoDB.cpp
@@ -6,7 +6,7 @@ MongoDB::MongoDB(const char* _uri_str) : uri_str(_uri_str)
 {
 }
 
-bool MongoDB::addCollection(const char* strCollection)
+void MongoDB::deleteCollection(const char* nameCollection)
 {
 	mongoc_client_t *client;
 	mongoc_collection_t *collection;
@@ -15,14 +15,61 @@ bool MongoDB::addCollection(const char* strCollection)
 	bson_t *doc;
 
 	mongoc_init();
+	bson_oid_init(&oid, NULL);
 
 	client = mongoc_client_new("mongodb://localhost:27017/?appname=insert-example");
-	collection = mongoc_client_get_collection(client, "mydb", strCollection);
+	collection = mongoc_client_get_collection(client, "SensorsDB", nameCollection);
+
+	doc = bson_new();
+	BSON_APPEND_OID(doc, "_id", &oid);
+
+	if (!mongoc_collection_remove(
+		collection, MONGOC_REMOVE_SINGLE_REMOVE, doc, NULL, &error)) {
+		fprintf(stderr, "Delete failed: %s\n", error.message);
+	}
+
+	bson_destroy(doc);
+	mongoc_collection_destroy(collection);
+	mongoc_client_destroy(client);
+	mongoc_cleanup();
+
+	/*bson_error_t error;
+	bson_oid_t oid;
+	bson_t *doc;
+
+	mongoc_init();
+
+	client = mongoc_client_new("mongodb://localhost:27017/?appname=delete-example");
+	collection = mongoc_client_get_collection(client, "mydb", "{\"name\": {\"second\":\"Grace1\", \"third\":\"Hopper1\"}}");
+
+	doc = bson_new();
+	BSON_APPEND_OID(doc, "_id", &oid);
+
+	if (!mongoc_collection_remove(
+		collection, MONGOC_REMOVE_SINGLE_REMOVE, doc, NULL, &error)) {
+		fprintf(stderr, "Delete failed: %s\n", error.message);
+	}
+
+	bson_destroy(doc);
+	mongoc_collection_destroy(collection);
+	mongoc_client_destroy(client);
+	mongoc_cleanup();*/
+}
+
+void MongoDB::addCollection(const char* nameCollection, const char* nameSensor, const char* valueSensor)
+{
+	bson_error_t error;
+	bson_oid_t oid;
+	bson_t *doc;
+	mongoc_init();
+
+	client = mongoc_client_new("mongodb://localhost:27017/?appname=insert-example");
+	collection = mongoc_client_get_collection(client, "SensorsDB", nameCollection);
 
 	doc = bson_new();
 	bson_oid_init(&oid, NULL);
 	BSON_APPEND_OID(doc, "_id", &oid);
-	BSON_APPEND_UTF8(doc, "hello", "world");
+	BSON_APPEND_UTF8(doc, nameSensor, valueSensor);
 
 	if (!mongoc_collection_insert(collection, MONGOC_INSERT_NONE, doc, NULL, &error)) {
 		fprintf(stderr, "%s\n", error.message);
@@ -33,9 +80,38 @@ bool MongoDB::addCollection(const char* strCollection)
 	mongoc_client_destroy(client);
 	mongoc_cleanup();
 
+		
 
+		//mongoc_init();
 
-	return false;
+		//client = mongoc_client_new("mongodb://localhost:27017/?appname=insert-example");
+		//collection = mongoc_client_get_collection(client, "mydb", strCollection);
+
+		//doc = bson_new();
+		//bson_oid_init(&oid, NULL);
+		//BSON_APPEND_OID(doc, "_id", &oid);
+		//BSON_APPEND_UTF8(doc, "hello", "world");
+
+		//if (!mongoc_collection_insert(collection, MONGOC_INSERT_NONE, doc, NULL, &error)) {
+		//	fprintf(stderr, "%s\n", error.message);
+		//	
+		//	ofstream outLogError("Log.txt");
+		//	char str[32]{};
+		//	time_t a = time(nullptr);
+		//	struct tm time_info;
+
+		//	// localtime_s, Microsoft version (returns zero on success, an error code on failure)
+		//	if (localtime_s(&time_info, &a) == 0) 
+		//		strftime(str, sizeof(str), "%H:%M:%S", &time_info);
+
+		//	outLogError << str << "> " << error.message <<"\n";
+		//	outLogError.close();
+		//}
+
+		//bson_destroy(doc);
+		//mongoc_collection_destroy(collection);
+		//mongoc_client_destroy(client);
+		//mongoc_cleanup();
 }
 
 
--- Reading for sensors/MongoDB.h
+++ Reading for sensors/MongoDB.h
@@ -1,16 +1,20 @@
 #pragma once
 #include "stdafx.h"
+using namespace std;
+
 class MongoDB
 {
 public:
 	MongoDB(const char* _uri_str);
-	bool addCollection(const char* strCollection);
+	void addCollection(const char* nameCollection, const char* nameSensor, const char* valueSensor);
+	void deleteCollection(const char* nameCollection);
 	~MongoDB();
 
 private:
 	const char *uri_str;
 	mongoc_client_t *client;
 	mongoc_database_t *database;
+	mongoc_collection_t *collection;
 
 	char *str;
 	bool retval;
